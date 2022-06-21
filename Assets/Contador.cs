cfa9bfc33b3b71394fef7609810b2e385ed27f45
fedelaborda <101400350+fedelaborda@users.noreply.github.com>
2022-06-21 14:18:49 -0300

    tyfuyuiu

--- Assets/Contador.cs
+++ Assets/Contador.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Contador : MonoBehaviour
 {
@@ -18,6 +19,9 @@ public class Contador : MonoBehaviour
     public Text ContadorText;
     public int TiempoToText;
 
+    public int golRojo;
+    public int golAzul;
+
     // Start is called before the first frame update
 
     void Start()
@@ -25,30 +29,59 @@ public class Contador : MonoBehaviour
         
     }
 
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Finish")
+        {
+            golRojo++;
+        }
+
+        if (col.gameObject.name == "ArcoAzul")
+        {
+            golAzul++;
+        }
+
+    }
+
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
     // Update is called once per frame
+
     void Update()
     {
         TimeToFinish -= Time.deltaTime;
         TiempoToText = (int)TimeToFinish;
         ContadorText.text = TiempoToText.ToString();
-        
 
-        if (TimeToFinish <= 0)
+
+        if (TimeToFinish <= 0 && golRojo > golAzul)
         {
-            RestartGame();
+            
+        }
+
+        if (TimeToFinish <= 0 && golRojo < golAzul)
+        {
+
+        }
+
+        if (TimeToFinish <= 0 && golRojo == golAzul)
+        {
+
         }
 
         //txt_time.text = Time.time.ToString();
     }
-    public void RestartGame()
-    {
-        transform.position = new Vector3(0, 0.57f, 0);
-        player1.transform.position = new Vector3(0, 0.5f, -10);
-        player1.transform.eulerAngles = new Vector3(0, 0, 0);
-        player2.transform.position = new Vector3(0, 0.5f, 10);
-        player2.transform.eulerAngles = new Vector3(0, 180, 0);
-        pelota.transform.position = new Vector3(0, 0.57f, 0);
-        TimeToFinish = 30f;
+    //public void RestartGame()
+    //{
+        //transform.position = new Vector3(0, 0.57f, 0);
+        //player1.transform.position = new Vector3(0, 0.5f, -10);
+        //player1.transform.eulerAngles = new Vector3(0, 0, 0);
+        //player2.transform.position = new Vector3(0, 0.5f, 10);
+        //player2.transform.eulerAngles = new Vector3(0, 180, 0);
+        //pelota.transform.position = new Vector3(0, 0.57f, 0);
+        //TimeToFinish = 30f;
          
-    }
+    //}
 }
